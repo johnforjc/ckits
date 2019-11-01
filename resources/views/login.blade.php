97b340d54853787c6084604f46f8a7d1128f3a18
johnforjc <44581271+johnforjc@users.noreply.github.com>
2019-11-01 15:13:44 +0700

    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -25,9 +25,28 @@
 
     <!-- Main CSS-->
     <link href="css/login.css" rel="stylesheet" media="all">
+    <link href="css/creative.min.css" rel="stylesheet" media="all">
 </head>
 
 <body>
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+        <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="/">CKITS</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto my-2 my-lg-0">
+            <li class="nav-item">
+                <a class="nav-link js-scroll-trigger" href="/login">Login</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link js-scroll-trigger" href="/register">Register</a>
+            </li>
+            </ul>
+        </div>
+        </div>
+    </nav>
 
     <div class="page-wrapper bg-gra-02 p-t-130 p-b-100 font-poppins">
         <div class="wrapper wrapper--w680">
