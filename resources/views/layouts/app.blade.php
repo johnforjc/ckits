4f68c506cf42572fb6374b3b5aae4fff8dea78f9
Fandi PJ <37364776+fandipj98@users.noreply.github.com>
2019-11-21 17:44:37 +0700

    view list kos

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -62,8 +62,10 @@
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="/users/{{ Auth::user()->id }}">{{ __('Profil') }}</a>
-                                    @if ( Auth::user()->status == '2')
+                                    @if (Auth::user()->status == '2')
                                         <a class="dropdown-item" href="/tempatkos/create">{{ __('Daftar Tempat Kos') }}</a>
+                                    @elseif (Auth::user()->status == '0')
+                                        <a class="dropdown-item" href="/users">{{ __('List User') }}</a>
                                     @endif
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
