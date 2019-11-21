4e2945c59ff12b30cc2b809ad68197064502f359
johnforjc <44581271+johnforjc@users.noreply.github.com>
2019-11-21 22:34:44 +0700

    Update listKost.blade.php

--- resources/views/listKost.blade.php
+++ resources/views/listKost.blade.php
@@ -1,43 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
-
-<table class="table">
-    <tr class="tr">
-        <th class="th">Nama Kos</th>
-        <th class="th">Alamat Kos</th>
-        <th class="th">Jumlah Kamar</th>
-        <th class="th">Harga</th>
-        <th class="th">Keterangan</th>
-        <th class="th">Foto</th>
-    </tr>
-    @foreach ($kosts as $kost)   
-        <div class="list-border">
-            <div class="list-box">
-                <img class='list-box-photo' src="/storage/image/{{$kost->foto_kos}}" alt="{{$kost->nama_tempat_kos}}">
-                <div class="list-box-text">
-                    <div class="form-group row">
-                        <label class="col-md-4 col-form-label text-md-right">{{ __('Nama Kos') }}</label>   
-                        <div class="col-md-6"> {{$kost->nama_tempat_kos}}</div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-4 col-form-label text-md-right">{{ __('Alamat Kos') }}</label>   
-                        <div class="col-md-6"> {{$kost->alamat}}</div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-4 col-form-label text-md-right">{{ __('Jumlah Kamar') }}</label>   
-                        <div class="col-md-6"> {{$kost->kamar_tersedia}}</div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-4 col-form-label text-md-right">{{ __('Harga Kos') }}</label>   
-                        <div class="col-md-6"> {{$kost->harga}}</div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-4 col-form-label text-md-right">{{ __('Keterangan') }}</label>   
-                        <div class="col-md-6">>{{$kost->keterangan_tempat_kos}}</div>
-                    </div>
+@foreach ($kosts as $kost)   
+    <div class="list-border">
+        <div class="list-box">
+            <img class='list-box-photo' src="/storage/image/{{$kost->foto_kos}}" alt="{{$kost->nama_tempat_kos}}">
+            <div class="list-box-text">
+                <div class="form-group row">
+                    <label class="col-md-4 col-form-label text-md-right">{{ __('Nama Kos') }}</label>   
+                    <div class="col-md-6"> {{$kost->nama_tempat_kos}}</div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-md-4 col-form-label text-md-right">{{ __('Alamat Kos') }}</label>   
+                    <div class="col-md-6"> {{$kost->alamat}}</div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-md-4 col-form-label text-md-right">{{ __('Jumlah Kamar') }}</label>   
+                    <div class="col-md-6"> {{$kost->kamar_tersedia}}</div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-md-4 col-form-label text-md-right">{{ __('Harga Kos') }}</label>   
+                    <div class="col-md-6"> {{$kost->harga}}</div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-md-4 col-form-label text-md-right">{{ __('Keterangan') }}</label>   
+                    <div class="col-md-6">>{{$kost->keterangan_tempat_kos}}</div>
                 </div>
             </div>
-        </div>               
-    @endforeach
+        </div>
+    </div>               
+@endforeach
 @endsection
